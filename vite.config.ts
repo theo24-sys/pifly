82487de8a08402d6129930a92ca29b21d520ac6e
Timothy Ekiru <timothybanjo42@gmail.com>
2025-11-12 13:15:12 +0300

    Fix all TS errors: main.tsx, App.tsx, socket.ts, vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 ﻿import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
 import path from 'path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
   base: '/',
   resolve: {
-    alias: { '@': path.resolve(__dirname, './src') },
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
   build: { outDir: 'dist', sourcemap: true },
   server: { port: 3000, open: true },
